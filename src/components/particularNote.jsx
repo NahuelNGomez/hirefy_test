3fb6520c5dd711622d9f652e87cd2c999f61d603
NahuelNGomez <nngomez@fi.uba.ar>
2024-07-06 12:20:29 -0300

    added dropdown

--- src/components/particularNote.jsx
+++ src/components/particularNote.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ParticularNote extends React.Component {
     render() {
-        // pt-[15px] pl-[10px]
+        
+        const sectionHeightClass = this.props.type === 'Feat' ? 'h-[443px]  md:h-[900px]' : 'h-auto  md:h-auto';
+        const colorType = this.props.type === 'Bug' ? 'bg-[#FFF8E5] text-[#FFB800]' : 'bg-[#D3FFF5] text-[#1CB59C]';
+        const index = this.props.index == 0 ? 'pt-[15px] pl-[10px]' : ' pl-[10px]';
         return (
-            <section className="w-[305px] h-[443px] flex  gap-[10px] md:w-[1100px] md:h-[900px]">
-                <section className='w-[18px] md:w-[35px] flex items-center justify-center relative'><hr className='border border-[#26E4C7] h-full' />
+            <section className={`w-[305px] ${sectionHeightClass} ${index} h-[443px] flex gap-[19px] md:w-[1100px]`}>
+                <section className='w-[18px] md:w-[35px] flex items-center justify-center relative '><hr className='border border-[#26E4C7] h-full ' />
                     <div className='absolute top-0 bg-[#26E4C7] rounded-full w-[18px] h-[18px]'></div>
                 </section>
                 <section className='w-[1000px] gap-[10px] flex flex-col'>
                     <section className='border-black gap-[10px] flex flex-col pr-[15px] pb-[20px]'>
-                        <h4 className='text-[17px] font-semibold text-[#2B3445] md:text-[36px] md:font-normal md:text-black'>Redesigned notification</h4>
+                        <h4 className='text-[17px] font-semibold text-[#2B3445] md:text-[36px] md:font-normal md:text-black'>{this.props.title}</h4>
                         <div className='flex gap-[10px] items-center'>
-                            <div className='rounded-[20px] px-[16px] py-[4px] font-bold bg-[#D3FFF5] text-[#1CB59C] text-[14px]'>Feat</div>
-                            <div className='text-[14px] text-[#7D879C]'>jul 15, 2024</div>
+                            <div className={`rounded-[20px] px-[16px] py-[4px] font-bold ${colorType} text-[14px]`}>{this.props.type}</div>
+                            <div className='text-[14px] text-[#7D879C]'>{this.props.day}</div>
                         </div>
-                        <div className='text-[14px] text-[#2B3445] md:text-[#7D879C]'>Customizing your notification settings is now easier than ever. Quickly change your settings using presets like Focused or Mentions only, or tailor them to fit your needs.</div>
+                        <div className='text-[14px] text-[#2B3445] md:text-[#7D879C]'>{this.props.description}</div>
                     </section>
-                    <img className="md:w-[974px]" src="image_9.png" alt="information" />
+                    {
+                        this.props.image && (
+                            <img className="md:w-[974px]" src={this.props.image} alt="information" />
+                        )
+                    }
+                    {
+                        this.props.bug_list && (
+                            this.props.bug_list.map((bug, index) => (
+                                <li key={index} className='font-medium text-[#2B3445] pb-1 md:pb-3 text-[14px]'>{bug}</li>
+                            ))
+                        )
+                    }
                 </section>
             </section>
         )
     }
 }
+
+ParticularNote.propTypes = {
+    index: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    day: PropTypes.arrayOf(PropTypes.string).isRequired,
+    type: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    bug_list: PropTypes.array,
+};
 
 export default ParticularNote;
--- src/components/releaseNotes.jsx
+++ src/components/releaseNotes.jsx
@@ -1,31 +1,102 @@
 import React from 'react';
 import ParticularNote from './particularNote';
+import data from '../assets/data.json';
 
 class ReleaseNotes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterType: 'All',
+            isDropdownOpen: false,
+        };
+        this.handleFilterChange = this.handleFilterChange.bind(this);
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    handleFilterChange(type) {
+        this.setState({ filterType: type, isDropdownOpen: false });
+    }
+
+    toggleDropdown() {
+        this.setState(prevState => ({ isDropdownOpen: !prevState.isDropdownOpen }));
+    }
+
     render() {
+        const filteredData = this.state.filterType === 'All'
+            ? data
+            : data.filter(note => note.type === this.state.filterType);
+
         return (
-            <section className="w-[305px] md:w-[1034px] justify-between flex flex-col ">
-                <div className=" flex flex-col md:flex-row gap-[10px] md:justify-between" >
+            <section className="w-[305px] md:w-[1034px] justify-between flex flex-col">
+                <div className="flex flex-col md:flex-row gap-[10px] md:justify-between">
                     <h3 className='text-[#2B3445] font-bold text-[20px] md:text-[44px] md:font-normal'>Release 6.5</h3>
-                    <div className='flex items-center gap-[10px]'>
-                        <h3 className='text-[14px] font-semibold'>Sort by:</h3>
-                        <div className='border h-[39px] w-[182px] flex justify-between rounded-[30px] items-center px-[20px]'>
-                            <p>All</p>
-                            <div><svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0.510249 3.47877L4.97278 7.99047C5.64474 8.66984 6.73023 8.66984 7.40219 7.99047L11.8647 3.47877C12.933 2.38133 12.1749 0.5 10.6414 0.5L1.73357 0.5C0.182881 0.499999 -0.575232 2.38133 0.510249 3.47877Z" fill="#0D5287" />
-                            </svg>
+                    <div className='relative'>
+                        <button
+                            id="desplegable"
+                            data-dropdown-toggle="dropdownDivider"
+                            className='flex items-center gap-[10px] border h-[39px] w-[182px] justify-between rounded-[30px] items-center px-[20px] bg-white'
+                            onClick={this.toggleDropdown}
+                        >
+                            <h3 className='text-[14px] font-semibold'>{this.state.filterType}</h3>
+                            <div>
+                                <svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.510249 3.47877L4.97278 7.99047C5.64474 8.66984 6.73023 8.66984 7.40219 7.99047L11.8647 3.47877C12.933 2.38133 12.1749 0.5 10.6414 0.5L1.73357 0.5C0.182881 0.499999 -0.575232 2.38133 0.510249 3.47877Z" fill="#0D5287" />
+                                </svg>
                             </div>
-                        </div>
+                        </button>
+                        {this.state.isDropdownOpen && (
+                            <div
+                                className="w-[182px] h-[117px] rounded-[20px] shadow-2xl absolute z-[10] bg-white"
+                                role="menu"
+                                aria-orientation="vertical"
+                                aria-labelledby="options-menu"
+                            >
+                                <div  role="none" className='h-full'>
+                                    <button
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#EFF7FF] hover:text-gray-900 w-full text-left"
+                                        role="menuitem"
+                                        onClick={() => this.handleFilterChange('All')}
+                                    >
+                                        All
+                                    </button>
+                                    <button
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#EFF7FF] hover:text-gray-900 w-full text-left"
+                                        role="menuitem"
+                                        onClick={() => this.handleFilterChange('Bug')}
+                                    >
+                                        Bug
+                                    </button>
+                                    <button
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#EFF7FF] hover:text-gray-900 w-full text-left"
+                                        role="menuitem"
+                                        onClick={() => this.handleFilterChange('Feat')}
+                                    >
+                                        Feat
+                                    </button>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
-                <section className='w-[305px] h-[22px] md:w-[1034px] flex flex-col justify-center pt-[5px]'><hr className='border border-[#DEE7F0]' />
+                <section className='w-[305px] h-[22px] md:w-[1034px] flex flex-col justify-center pt-[5px]'>
+                    <hr className='border border-[#DEE7F0]' />
                 </section>
                 <div className='flex flex-col'>
-                    <ParticularNote />
-                    <ParticularNote />
+                    {filteredData.map((note, index) => (
+                        <ParticularNote
+                            key={index}
+                            index={index}
+                            title={note.title}
+                            day={note.day}
+                            type={note.type}
+                            image={note.image}
+                            description={note.description}
+                            bug_list={note.bug_list}
+                        />
+                    ))}
                 </div>
             </section>
-        )
+        );
     }
 }
 
